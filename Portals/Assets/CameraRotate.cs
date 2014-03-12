121e1c0dc884b4985aa049dcdaa2708bddc649f9
Michael Tesija <mtesija@umich.edu>
2014-03-12 04:13:48 -0400

    stuff

---
+++ Portals/Assets/CameraRotate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotate : MonoBehaviour
+{
+	private GameObject mainCam;
+	public GameObject enterPortal;
+	public GameObject exitPortal;
+	
+	void Start()
+	{
+		mainCam = GameObject.Find("Main Camera");
+	}
+
+	void Update()
+	{
+		Vector3 result = mainCam.transform.forward + enterPortal.transform.up;
+		result.Normalize();
+		result = exitPortal.transform.up + result;
+		result.Normalize();
+
+		this.transform.eulerAngles = result;
+	}
+}
--- Portals/Assets/RenderCameraTexture.cs
+++ Portals/Assets/RenderCameraTexture.cs
@@ -3,7 +3,13 @@ using System.Collections;
 
 public class RenderCameraTexture : MonoBehaviour 
 {
-	Camera cam;
+	public Camera cam;
+	private GameObject mainCam;
+
+	void Start()
+	{
+		mainCam = GameObject.Find("Main Camera");
+	}
 
 	void Update()
 	{
@@ -14,13 +20,17 @@ public class RenderCameraTexture : MonoBehaviour
 	{
 		yield return new WaitForEndOfFrame();
 
+		cam.Render();
+
 		Texture2D image = new Texture2D(Screen.width, Screen.height);
 
-		image.ReadPixels(new Rect(Screen.width, Screen.height), 0, 0, false);
+		image.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0, false);
 
 		image.Apply();
 
 		renderer.material.mainTexture = image;
+
+		mainCam.camera.Render();
 
 		yield break;
 	}
