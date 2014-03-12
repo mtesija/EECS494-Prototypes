2473ded69f741e8eba77409a38a9fbcf2ba8bba4
Michael Tesija <mtesija@umich.edu>
2014-03-11 21:19:35 -0400

    commit

---
+++ Portals/Assets/RenderCameraTexture.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RenderCameraTexture : MonoBehaviour 
+{
+	Camera cam;
+
+	void Update()
+	{
+		StartCoroutine(Capture());
+	}
+
+	private IEnumerator Capture()
+	{
+		yield return new WaitForEndOfFrame();
+
+		Texture2D image = new Texture2D(Screen.width, Screen.height);
+
+		image.ReadPixels(new Rect(Screen.width, Screen.height), 0, 0, false);
+
+		image.Apply();
+
+		renderer.material.mainTexture = image;
+
+		yield break;
+	}
+}
